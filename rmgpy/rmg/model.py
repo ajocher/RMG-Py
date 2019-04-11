a23c8a9ddfee62338c88e55bbb53255ea41e30c8
Agnes <ajocher@mit.edu>
2019-04-11 14:46:47 -0400

    Alternate approach to QMTP parallelization

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -69,32 +69,6 @@ from pdep import PDepReaction, PDepNetwork
 
 ################################################################################
 
-def calculate_thermo_parallel(spc):
-    """
-    If quantumMechanics is turned on in the input file species thermo data is calculated 
-    in this function.
-    """
-
-    from rmgpy.rmg.input import getInput
-
-    try:
-        quantumMechanics = getInput('quantumMechanics')
-    except Exception:
-        logging.debug('Quantum Mechanics DB could not be found.')
-        quantumMechanics = None
-
-    spc.generate_resonance_structures()
-    original_molecule = spc.molecule[0]
-
-    if quantumMechanics.settings.onlyCyclics and not original_molecule.isCyclic():
-        pass
-    else: 
-        if original_molecule.getRadicalCount() > quantumMechanics.settings.maxRadicalNumber:
-            pass
-        else: 
-            logging.info('Not too many radicals: do a direct QM calculation.')
-            thermo0 = quantumMechanics.getThermoData(original_molecule) # returns None if it fails
-
 class ReactionModel:
     """
     Represent a generic reaction model. A reaction model consists of `species`,
@@ -332,7 +306,7 @@ class CoreEdgeReactionModel:
         # At this point we can conclude that the structure does not exist
         return False, False, None
 
-    def makeNewSpecies(self, object, label='', reactive=True, checkForExisting=True):
+    def makeNewSpecies(self, object, label='', reactive=True, checkForExisting=True, generateThermo=True):
         """
         Formally create a new species from the specified `object`, which can be
         either a :class:`Molecule` object or an :class:`rmgpy.species.Species`
@@ -397,25 +371,11 @@ class CoreEdgeReactionModel:
             spec.generate_resonance_structures()
         spec.molecularWeight = Quantity(spec.molecule[0].getMolecularWeight()*1000.,"amu")
 
-        if not spec.thermo:
-            submit(spec,self.solventName)
+        if generateThermo:
+            self.generateThermo(spec)
 
-        if spec.label == '':
-            if spec.thermo and spec.thermo.label != '': #check if thermo libraries have a name for it
-                logging.info('Species with SMILES of {0} named {1} based on thermo library name'.format(molecule.toSMILES().replace('/','').replace('\\',''),spec.thermo.label))
-                spec.label = spec.thermo.label
-                label = spec.label
-            else:
-                # Use SMILES as default format for label
-                # However, SMILES can contain slashes (to describe the
-                # stereochemistry around double bonds); since RMG doesn't
-                # distinguish cis and trans isomers, we'll just strip these out
-                # so that we can use the label in file paths
-                label = molecule.toSMILES().replace('/','').replace('\\','')
+        logging.debug('Creating new species {0}'.format(spec.label))
 
-        logging.debug('Creating new species {0}'.format(label))
-
-        spec.generateEnergyTransferModel()
         formula = molecule.getFormula()
         if formula in self.speciesDict:
             self.speciesDict[formula].append(spec)
@@ -513,7 +473,7 @@ class CoreEdgeReactionModel:
 
         return False, None
 
-    def makeNewReaction(self, forward, checkExisting=True):
+    def makeNewReaction(self, forward, checkExisting=True, generateThermo=True):
         """
         Make a new reaction given a :class:`Reaction` object `forward`.
         The reaction is added to the global list of reactions.
@@ -529,8 +489,8 @@ class CoreEdgeReactionModel:
         """
 
         # Determine the proper species objects for all reactants and products
-        reactants = [self.makeNewSpecies(reactant)[0] for reactant in forward.reactants]
-        products  = [self.makeNewSpecies(product)[0]  for product  in forward.products ]
+        reactants = [self.makeNewSpecies(reactant, generateThermo=generateThermo)[0] for reactant in forward.reactants]
+        products  = [self.makeNewSpecies(product, generateThermo=generateThermo)[0]  for product  in forward.products ]
         if forward.specificCollider is not None:
             forward.specificCollider = self.makeNewSpecies(forward.specificCollider)[0]
 
@@ -655,7 +615,7 @@ class CoreEdgeReactionModel:
                 pdepNetwork, newSpecies = newObject
                 newReactions.extend(pdepNetwork.exploreIsomer(newSpecies))
 
-                self.processNewReactions(newReactions, newSpecies, pdepNetwork)
+                self.processNewReactions(newReactions, newSpecies, pdepNetwork, generateThermo=False)
 
             else:
                 raise TypeError('Unable to use object {0} to enlarge reaction model; expecting an object of class rmg.model.Species or rmg.model.PDepNetwork, not {1}'.format(newObject, newObject.__class__))
@@ -676,7 +636,7 @@ class CoreEdgeReactionModel:
                         if len(products) == 1 and products[0] == species:
                             newReactions = network.exploreIsomer(species)
 
-                            self.processNewReactions(newReactions, species, network)
+                            self.processNewReactions(newReactions, species, network, generateThermo=False)
                             network.updateConfigurations(self)
                             index = 0
                             break
@@ -695,56 +655,25 @@ class CoreEdgeReactionModel:
                              unimolecularReact, bimolecularReact, trimolecularReact=trimolecularReact)
 
             # Get new species and save in spcs
-            spcs_tmp = []
+            spcs = []
+            spcs_list = []
             for rxn in rxns:
-                spcs_tmp.extend(rxn.reactants)
-                spcs_tmp.extend(rxn.products)
-                
-            spcs = spcs_tmp
-
-            from rmgpy.rmg.input import getInput
-            try:
-                quantumMechanics = getInput('quantumMechanics')
-            except Exception:
-                logging.debug('Quantum Mechanics DB could not be found.')
-                quantumMechanics = None
-        
-            if not quantumMechanics:
-                pass
-            else:
-                if not spcs_tmp:
-                    spcs = spcs_tmp
-                else:
-                    # Generate unique list of species to be submitted to QM thermo calculation
-                    # intilize list 
-                    spcs=[spcs_tmp[0]]
-                    for counter, spc in enumerate (spcs_tmp):
-                        for counter2, val in enumerate (spcs):
-                            if (spc.molecule[0].toSMILES() != val.molecule[0].toSMILES()):
-                                appendReactant = True
-                            else:
-                                appendReactant = False
-                                break
-                        if appendReactant:
-                            spcs.append(spc)
-    
-                    procnum = determine_procnum_from_RAM()
-
-                    # Execute multiprocessing map. It blocks until the result is ready.
-                    # This method chops the iterable into a number of chunks which it
-                    # submits to the process pool as separate tasks.
-                    p = Pool(processes=procnum)
-                    p.map(calculate_thermo_parallel,spcs)
-                    p.close()
-                    p.join()
-
-            ensure_independent_atom_ids(spcs, resonance=True)
+                spcs.extend(rxn.reactants)
+                spcs.extend(rxn.products)
 
             for rxn, spc in zip(rxns, spcs):
-               self.processNewReactions([rxn], spc)
+               self.processNewReactions([rxn], spc, generateThermo=False)
 
         ################################################################
         # Begin processing the new species and reactions
+
+        # Determine number of parallel processes.
+        procnum = determine_procnum_from_RAM()
+
+        # Generate thermo for new species
+        if self.newSpeciesList:
+            logging.info('Generating thermo for new species...')
+            self.applyThermoToSpecies(procnum)
 
         # Generate kinetics of new reactions
         if self.newReactionList:
@@ -855,7 +784,7 @@ class CoreEdgeReactionModel:
         self.newSurfaceSpcsLoss = set()
         self.newSurfaceRxnsLoss = set()
 
-    def processNewReactions(self, newReactions, newSpecies, pdepNetwork=None):
+    def processNewReactions(self, newReactions, newSpecies, pdepNetwork=None, generateThermo=True):
         """
         Process a list of newly-generated reactions involving the new core
         species or explored isomer `newSpecies` in network `pdepNetwork`.
@@ -863,7 +792,7 @@ class CoreEdgeReactionModel:
         Makes a reaction and decides where to put it: core, edge, or PDepNetwork.
         """
         for rxn in newReactions:
-            rxn, isNew = self.makeNewReaction(rxn)
+            rxn, isNew = self.makeNewReaction(rxn, generateThermo=generateThermo)
             if rxn is None:
                 # Skip this reaction because there was something wrong with it
                 continue
@@ -934,6 +863,61 @@ class CoreEdgeReactionModel:
             if not numpy.isinf(self.toleranceThermoKeepSpeciesInEdge) and spcs != []: #do thermodynamic filtering
                 self.thermoFilterSpecies(spcs)
 
+    def applyThermoToSpecies(self, procnum):
+        """
+        Generate thermo for species. QM calculations are parallelized if requested.
+        """
+        from rmgpy.rmg.input import getInput
+        quantumMechanics = getInput('quantumMechanics')
+
+        if quantumMechanics:
+            # Generate a list of molecules.
+            mol_list = []
+            for spc in self.newSpeciesList:
+                if spc.molecule[0].getRadicalCount() > quantumMechanics.settings.maxRadicalNumber:
+                    for molecule in spc.molecule:
+                        if quantumMechanics.settings.onlyCyclics and molecule.isCyclic():
+                            saturated_mol = molecule.copy(deep=True)
+                            saturated_mol.saturate_radicals()
+                            if saturated_mol not in mol_list:
+                                mol_list.append(saturated_mol)
+                else:
+                    if quantumMechanics.settings.onlyCyclics and spc.molecule[0].isCyclic():
+                        if spc.molecule[0] not in mol_list:
+                            mol_list.append(spc.molecule[0])
+            if procnum == 1:
+                logging.info('Writing QM files with {0} process.'.format(procnum))
+                map(quantumMechanics.getThermoData, mol_list)
+            else:
+                logging.info('Writing QM files with {0} processes.'.format(procnum))
+                p = Pool(processes=procnum)
+                p.map(quantumMechanics.getThermoData, mol_list)
+                p.close()
+                p.join()
+
+        # Serial thermo calculation for other methods
+        map(self.generateThermo, self.newSpeciesList)
+
+    def generateThermo(self, spc):
+        """
+        Generate thermo for species.
+        """
+        if not spc.thermo:
+            submit(spc, self.solventName)
+
+        if spc.label == '':
+            if spc.thermo and spc.thermo.label != '': #check if thermo libraries have a name for it
+                logging.info('Species with SMILES of {0} named {1} based on thermo library name'.format(spc.molecule[0].toSMILES().replace('/','').replace('\\',''), spc.thermo.label))
+                spc.label = spc.thermo.label
+            else:
+                # Use SMILES as default format for label
+                # However, SMILES can contain slashes (to describe the
+                # stereochemistry around double bonds); since RMG doesn't
+                # distinguish cis and trans isomers, we'll just strip these out
+                # so that we can use the label in file paths
+                spc.label = spc.molecule[0].toSMILES().replace('/','').replace('\\','')
+
+        spc.generateEnergyTransferModel()
 
     def applyKineticsToReaction(self, reaction):
         """
